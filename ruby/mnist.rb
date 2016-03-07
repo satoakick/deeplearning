f3b827a56331a8043a1978adb23bee803b74ae64
hetakan <hetakan@gmail.com>
2016-03-08 00:54:33 +0900

    The script which converts the 28*28 data to 784 data(by one line) makes.

--- ruby/mnist.rb
+++ ruby/mnist.rb
@@ -1,15 +1,21 @@
-#file_path = '/Users/sawadasatoaki/Downloads/train-images-idx3-ubyte'
-file_path = '/Users/sawadasatoaki/Downloads/train-labels-idx1-ubyte'
-f = open(file_path, "rb")
+file_path = '/Users/sawadasatoaki/Downloads/train-images-idx3-ubyte'
+#file_path = '/Users/sawadasatoaki/Downloads/train-labels-idx1-ubyte'
 
-
-count = 0
-
-train_label
-f.each_byte do |c|
-  printf"%02X ",c
-
-  count += 1
+count = 1
+line = 1
+p "file write begin."
+File.open("train-images-idx3-ubyte.txt","w") do |file|
+  f = open(file_path, "rb")
+  f.each_byte do |c|
+      byte = "%02X"%c
+      file.print("#{byte} ") if count > 784 # 最初の16バイトはノーカン(see http://yann.lecun.com/exdb/mnist/)
+      if count > 784 && count % 784 == 0 # 改行追加
+        file.puts ""
+        line += 1
+        puts "#{line}行目"
+      end
+    count += 1
+  end
+  f.close
 end
-puts ""
-f.close
+p "file write end."
