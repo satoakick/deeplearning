eee7365ca608ae05796bf0be52d9e7065d350c35
satoakick <satoakick@gmail.com>
2016-03-12 21:00:34 +0900

    - fix some of the comments. - fix the output filename by getting from the input filename.

--- ruby/mnist.rb
+++ ruby/mnist.rb
@@ -1,18 +1,25 @@
-file_path = '../train-images-idx3-ubyte'
-#file_path = '../train-labels-idx1-ubyte'
+# [remark] Assume that there is the Leaning data and Test Data files in current dir.
+
+# This is the script which converts 28*28 to 764 dimension vector.
+# The more detail format is http://yann.lecun.com/exdb/mnist/.
+
+filename = 'train-images-idx3-ubyte' # Learning data
+#filename = 'train-labels-idx1-ubyte' # Test data
+
 
 count = 1
 line = 1
 p "file write begin."
-File.open("train-images-idx3-ubyte.txt","w") do |file|
+
+File.open("#{filename.split('.')[0]}.txt","w") do |file| # assume that file is in current dir!
   f = open(file_path, "rb")
   f.each_byte do |c|
       byte = "%02X"%c
-      file.print("#{byte} ") if count > 784 # 最初の16バイトはノーカン(see http://yann.lecun.com/exdb/mnist/)
-      if count > 784 && count % 784 == 0 # 改行追加
+      file.print("#{byte} ") if count > 784 # skip the first 16bytes(see http://yann.lecun.com/exdb/mnist/)
+      if count > 784 && count % 784 == 0 # add the line break
         file.puts ""
         line += 1
-        puts "#{line}行目"
+        puts "the #{line} -th "
       end
     count += 1
   end
