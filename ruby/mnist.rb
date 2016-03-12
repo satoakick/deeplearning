b04b68a77eb36ac99698f0a212159e3608bf1374
satoakick <satoakick@gmail.com>
2016-03-12 23:27:12 +0900

    use the IO.binread for parsing the traing data

--- ruby/mnist.rb
+++ ruby/mnist.rb
@@ -1,28 +1,37 @@
-# [remark] Assume that there is the Leaning data and Test Data files in current dir.
-
+# [Remark] Assume that there is the Leaning data and Test Data files in current dir.
 # This is the script which converts 28*28 to 764 dimension vector.
 # The more detail format is http://yann.lecun.com/exdb/mnist/.
 
-filename = 'train-images-idx3-ubyte' # Learning data
-#filename = 'train-labels-idx1-ubyte' # Test data
-
-
-count = 1
-line = 1
-p "file write begin."
-
-File.open("#{filename.split('.')[0]}.txt","w") do |file| # assume that file is in current dir!
-  f = open(file_path, "rb")
-  f.each_byte do |c|
-      byte = "%02X"%c
-      file.print("#{byte} ") if count > 784 # skip the first 16bytes(see http://yann.lecun.com/exdb/mnist/)
-      if count > 784 && count % 784 == 0 # add the line break
+def parse_training_data(input_filename, offset, number_by_a_line)
+  count = 1
+  line = 1
+  p "file write begin."
+  p "parse the image training data: #{input_filename}"
+  File.open("#{input_filename}.txt","w") do |file| # assume that original file is in current dir!
+    f = IO.binread(input_filename, nil, offset) # skip #{offset}bytes!
+    f.each_byte do |c|
+      file.print("#{'%02X'%c}")
+      file.print(" ") if count % number_by_a_line != 0
+      if count % number_by_a_line == 0 # add the line break
         file.puts ""
+        p "#{line}-th"
         line += 1
-        puts "the #{line} -th "
       end
-    count += 1
+      count += 1
+    end
   end
-  f.close
+  p "file write end."
 end
-p "file write end."
+
+if ARGV[0] && ARGV[1]
+  # Input data
+  input_file_name_training_set_image = ARGV[0] # Training data of image
+  input_file_name_training_set_label = ARGV[1] # Training data of label
+
+  # parse
+  parse_training_data(input_file_name_training_set_image, 16, 784)
+  parse_training_data(input_file_name_training_set_label, 8, 1)
+else
+  p 'first arg is Training data of image'
+  p 'second arg is Training data of label'
+end
