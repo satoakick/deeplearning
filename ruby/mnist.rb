8f9e64263756338083429b15fe10850bd69f8ee5
satoakick <satoakick@gmail.com>
2016-03-13 13:12:04 +0900

    fix variables : count => count_by_a_byte, line => count_by_a_line

--- ruby/mnist.rb
+++ ruby/mnist.rb
@@ -2,22 +2,22 @@
 # This is the script which converts 28*28 to 764 dimension vector.
 # About the more detail format, see http://yann.lecun.com/exdb/mnist/.
 
-def parse_training_data(input_filename, offset, number_by_a_line)
-  count = 1
-  line = 1
+def parse_training_data(input_filename, offset, number_by_a_count_by_a_line)
+  count_by_a_byte = 1
+  count_by_a_line = 1
   p "file write begin."
   p "parse the image training data: #{input_filename}"
   File.open("#{input_filename}.txt","w") do |file| # assume that original file is in current dir!
     f = IO.binread(input_filename, nil, offset) # skip #{offset}bytes!
     f.each_byte do |c|
       file.print("#{'%02X'%c}")
-      file.print(" ") if count % number_by_a_line != 0
-      if count % number_by_a_line == 0 # add the line break
+      file.print(" ") if count_by_a_byte % number_by_a_count_by_a_line != 0
+      if count_by_a_byte % number_by_a_count_by_a_line == 0 # add the line break
         file.puts ""
-        p "#{line}-th"
-        line += 1
+        p "#{count_by_a_line}-th"
+        count_by_a_line += 1
       end
-      count += 1
+      count_by_a_byte += 1
     end
   end
   p "file write end."
